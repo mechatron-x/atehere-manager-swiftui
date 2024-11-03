e0024b355e7b3a16b5af0ffa14325c3c81289c4f
Berke Bozacı <bbozaci20@ku.edu.tr>
2024-11-03 11:56:22 +0300

    Initial Commit

---
+++ atehere-manager-swiftui/atehere_manager_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  atehere_manager_swiftuiApp.swift
+//  atehere-manager-swiftui
+//
+//  Created by Berke Bozacı on 3.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct atehere_manager_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
