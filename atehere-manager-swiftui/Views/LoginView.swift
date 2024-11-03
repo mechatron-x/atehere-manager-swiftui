a245eb6738b145c05f018b2154f4260fa15cb8f0
Berke Bozacı <bbozaci20@ku.edu.tr>
2024-11-03 17:36:16 +0300

    dummy ui removed

--- atehere-manager-swiftui/Views/LoginView.swift
+++ atehere-manager-swiftui/Views/LoginView.swift
@@ -8,53 +8,16 @@
 import SwiftUI
 
 struct LoginView: View {
-    @StateObject private var viewModel = LoginViewModel()
-
-        var body: some View {
-            VStack {
-                TextField("Email", text: $viewModel.email)
-                    .keyboardType(.emailAddress)
-                    .autocapitalization(.none)
-                    .padding()
-                    .background(Color(.secondarySystemBackground))
-                    .cornerRadius(5)
-
-                SecureField("Password", text: $viewModel.password)
-                    .padding()
-                    .background(Color(.secondarySystemBackground))
-                    .cornerRadius(5)
-
-                if let errorMessage = viewModel.errorMessage {
-                    Text(errorMessage)
-                        .foregroundColor(.red)
-                        .multilineTextAlignment(.center)
-                        .padding()
-                }
-
-                Button(action: {
-                    viewModel.login()
-                }) {
-                    Text("Login")
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.blue)
-                        .cornerRadius(5)
-                }
-                .padding()
-                .disabled(viewModel.isLoading)
-
-                if viewModel.isLoading {
-                    ProgressView()
-                        .padding()
-                }
-            }
-            .padding()
-            .navigationTitle("Manager Login")
-            .fullScreenCover(isPresented: $viewModel.isAuthenticated) {
-                ContentView()
-            }
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
+        .padding()
+    }
 }
 
 #Preview {
