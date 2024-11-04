e1c4d1ca15cb8334f2c5835d63c933d6f1d4c73d
Pelin Önen <pelinonen@192.168.1.105>
2024-11-04 23:49:21 +0300

    manager login

---
+++ atehere-manager-swiftui/Views/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  atehere-manager-swiftui
+//
+//  Created by Pelin Önen on 4.11.2024.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
